6880d44a7de4b59152c8c7895b7b9127b2c77bd7
mjditter <40790983-mjditter@users.noreply.replit.com>
2025-06-04 11:32:17 +0000

    Add the ability to show up-to-date resource locations on the interactive map
    Introduces useState and useEffect hooks in ResourceMarkers.tsx to manage map resource updates.

--- client/src/components/map/ResourceMarkers.tsx
+++ client/src/components/map/ResourceMarkers.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Truck, Building, Users, Package } from 'lucide-react';
 import { ResourceDeployment } from '@/types/geo.types';
 
